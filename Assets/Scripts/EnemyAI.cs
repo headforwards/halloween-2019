11616965a75b030b3c241753d31e5853e70fe9b5
Barney Nicholls <barneynicholls@Headforwardss-iMac-3.local>
2019-09-27 13:47:59 +0100

    scene demo navmesh navigation and sample vr scene

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    public Transform Target;
+    // Start is called before the first frame update    
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(Target.position);
+    }
+}
