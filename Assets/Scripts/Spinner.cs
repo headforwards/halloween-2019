5eefa09c912590d242c69d717caf21130563af9f
barneynicholls <barney.nicholls@bluetoad.net>
2019-09-29 21:57:54 +0100

    updated scenes

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public float Spin = 90;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Spin * Time.deltaTime);
+    }
+
+    public void ChangeDirection()
+    {
+        Spin = -Spin;
+    }
+}
